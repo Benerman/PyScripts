8dc76b921b291a1f2ac785d2012b3e064a2919c7
Ben Larson <benjaminwlarson@gmail.com>
2019-10-13 16:49:10 -0400

    update

---
+++ rename-files.py
@@ -0,0 +1,8 @@
+
+import os
+from pathlib import Path
+import tkinter as tk
+
+root = tk.Tk()
+root.withdraw()
+
--- sd-card-watcher.py
+++ sd-card-watcher.py
@@ -10,20 +10,49 @@
 
 import os, time
 
+def moinitor_drive():
+	pass
+	# move_files()
+
+def move_files():
+	pass
+	# monitor_drive()
+
 def main():
 	pass
 # 	source = get watch drive
 # 	dest = get folder to transfer files to
+
+	# move_files()
 	while True:
 		if card in drive:
+			# os walk
+			# OR
+			# os.scandir
+			# with os.scandir(path) as it:
+			# 	count = 0
+			# 	for entry in it:
+			# 		if count > 5:
+			# 			break
+			# 		if not entry.name.startswith('.') and entry.is_file():
+			# 			print(entry.name)
+			# 			print(datetime.datetime.fromtimestamp(int(entry.stat().st_ctime)))
+			# 			count += 1
 			for image in files_on_card:
-				all_images.extend(image)
+				# image check needed '.CR2' or '.JPG'
+				# list of files in the folder
+				all_images.extend([image])
+				# file information check, Get date modified or created
+				# parse image_date to be OS friendly
 				image_date_set.add(image.date)
 			if image_date_set > 0:
 				if not path isdir(os.path.join(dest, date_folder):
 					os.mkdir(image_date)
 			files_to_transfer = all_images.pop()
-			copy file to dest + date_folder
+			folder_loc = os.path.join(dest, date_folder)
+		#	# Copy all files to folder_loc
+
+			print(f'{len(files_to_transfer)} out of {len(files in date_folder)}')
 		else:	
 			time.sleep(5)
 
