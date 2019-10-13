b692d25350a641b5427c6e6aeeeb76b900bd5f6a
Benerman <48377757+Benerman@users.noreply.github.com>
2019-10-13 13:24:44 -0400

    Create sd-card-watcher.py

---
+++ sd-card-watcher.py
@@ -0,0 +1,32 @@
+
+# watch specific drive
+# when image files are on disk
+# compare files on card to files on disk
+# if not folder exist
+# 	create folder with date
+# those that arent on disk
+# 	transfer iamges to location
+
+
+import os, time
+
+def main():
+	pass
+# 	source = get watch drive
+# 	dest = get folder to transfer files to
+	while True:
+		if card in drive:
+			for image in files_on_card:
+				all_images.extend(image)
+				image_date_set.add(image.date)
+			if image_date_set > 0:
+				if not path isdir(os.path.join(dest, date_folder):
+					os.mkdir(image_date)
+			files_to_transfer = all_images.pop()
+			copy file to dest + date_folder
+		else:	
+			time.sleep(5)
+
+
+if __name__ == '__main__':
+	main()
